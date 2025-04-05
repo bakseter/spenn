66f6c7f9e6eb83e911af3332ec624a7c2f260dd6
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2025-04-05 18:49:30 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM golang:alpine AS build
+
+ENV GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+RUN go build -o ./out/executable .
+
+
+FROM alpine:latest
+
+RUN apk update && \
+    apk upgrade --no-cache
+
+RUN addgroup application-group --gid 1001 && \
+    adduser application-user --uid 1001 \
+        --ingroup application-group \
+        --disabled-password
+
+WORKDIR /app
+
+COPY --from=build /app/out .
+
+RUN chown --recursive application-user .
+USER application-user
+
+EXPOSE 8080
+
+COPY static ./static
+COPY templates ./templates
+
+ENTRYPOINT ["./executable"]
---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type TransactionJSON struct {
+	Amount      int    `json:"amount,string"`
+	Description string `json:"description"`
+}
+
+type TransactionDB struct {
+	gorm.Model
+	Amount      int
+	Description string
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile("./static", true)))
+    router.LoadHTMLGlob("templates/*")
+
+	databaseHost := os.Getenv("DATABASE_HOST")
+	if databaseHost == "" {
+		databaseHost = "localhost"
+	}
+
+	databaseUser := os.Getenv("DATABASE_USER")
+	if databaseUser == "" {
+		panic("DATABASE_USER is not set")
+	}
+
+	databasePassword := os.Getenv("DATABASE_PASSWORD")
+	if databasePassword == "" {
+		panic("DATABASE_PASSWORD is not set")
+	}
+
+	databaseName := os.Getenv("DATABASE_NAME")
+	if databaseName == "" {
+		panic("DATABASE_NAME is not set")
+	}
+
+	dataSourceName := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Europe/Oslo",
+        databaseHost,
+		databaseUser,
+		databasePassword,
+		databaseName,
+	)
+
+	database, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{})
+	if err != nil {
+        fmt.Errorf("failed to connect to database: %v", err)
+	}
+
+	database.AutoMigrate(&TransactionDB{})
+
+	api := router.Group("/api")
+	{
+		api.GET("/status", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"status": "ok",
+			})
+		})
+
+		api.POST("/transaction", func(c *gin.Context) {
+			var transaction TransactionJSON
+			if err := c.ShouldBindJSON(&transaction); err != nil {
+				c.JSON(400, gin.H{"error": err.Error()})
+				return
+			}
+
+			dbTransaction := TransactionDB{
+				Amount:      transaction.Amount,
+				Description: transaction.Description,
+			}
+			if err := database.Create(&dbTransaction).Error; err != nil {
+				c.JSON(500, gin.H{"error": "failed to save transaction"})
+				return
+			}
+
+			c.Header("Content-Type", "text/html")
+			c.String(200, "<p>transaction received :)</p>")
+		})
+
+		api.GET("/transactions", func(c *gin.Context) {
+			var transactions []TransactionDB
+			if err := database.Find(&transactions).Error; err != nil {
+				c.JSON(500, gin.H{"error": "failed to fetch transactions"})
+				return
+			}
+
+			if len(transactions) == 0 {
+				c.JSON(200, gin.H{"message": "no transactions found"})
+				return
+			}
+
+			var transactionList []TransactionJSON
+			for _, transaction := range transactions {
+				transactionList = append(transactionList, TransactionJSON{
+					Amount:      transaction.Amount,
+					Description: transaction.Description,
+				})
+			}
+
+			c.HTML(200, "transactions.html.tmpl", gin.H{
+                "Transactions": transactionList,
+            })
+		})
+	}
+
+	router.Run(":8080")
+}
