6ebdfa3256597450cea4dd6c3e2b56a5b33508dc
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2025-04-12 21:06:13 +0200

    Add CORS

--- main.go
+++ main.go
@@ -2,18 +2,58 @@ package main
 
 import (
 	"log"
+	"os"
+	"time"
 
 	"github.com/bakseter/spenn/pkg/models"
 	"github.com/bakseter/spenn/pkg/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func main() {
+	dev := func() bool {
+		dev_ := os.Getenv("DEV")
+
+		return dev_ == "true"
+	}()
+
 	router := gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./static", true)))
 	router.LoadHTMLGlob("templates/*")
+
+	if !dev {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	if !dev {
+		host := os.Getenv("HOST")
+		router.Use(cors.New(cors.Config{
+			AllowOrigins: []string{host},
+			AllowMethods: []string{"GET", "PUT", "POST", "DELETE"},
+			AllowHeaders: []string{
+				"Origin",
+				"Content-Type",
+				"Accept",
+				"Authorization",
+				"X-Auth-Request-User",
+				"X-Auth-Request-Email",
+				"X-Auth-Requiest-Groups",
+				"X-Auth-Request-Access-Token",
+				"X-Auth-Request-Preferred-Username",
+				"X-Forwarded-Access-Token",
+				"X-Forwarded-User",
+				"X-Forwarded-Email",
+				"X-Forwarded-Preferred-Username",
+				"X-Forwarded-Groups",
+			},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		}))
+	}
 
 	database, err := models.InitializeDatabase()
 	if err != nil {
